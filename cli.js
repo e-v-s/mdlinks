d01a01be0415e397c6ef68e7f92e3cccef59e7f7
evs <evoraleite@gmail.com>
2019-11-28 11:15:32 -0300

    library working

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
-const mdlinks = require('mdlinks');
+#!/usr/bin/env node
+const args = process.argv.slice(2);
+const mdlinks = require('./lib/index.js');
+const path = require('path');
 
-mdlinks()
-	.then()
-	.catch(console.error)
+mdlinks(args[0]).then(obj => console.log(obj)).catch(console.error)
+	
