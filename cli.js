7ebc062efbc31976a25ddb15a21005aae9aad0d1
evs <evoraleite@gmail.com>
2019-11-30 15:04:31 -0300

    hacker edition, validation working but not as asked

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-
 const args = process.argv.slice(2)
 const mdlinks = require('./lib/index.js')
+//const validateLinks	= require('./lib/index.js')
 
-mdlinks(args[0])
+mdlinks(args[0], args[1])
   .then(obj => console.log(obj))
   .catch(err => console.log(err))
