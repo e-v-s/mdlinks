c0b2c94239911feea93c463da6653c23419c40ce
evs <evoraleite@gmail.com>
2019-12-02 14:02:05 -0300

    refatorado

--- cli.js
+++ cli.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const args = process.argv.slice(2)
 const mdlinks = require('./lib/index.js')
-//const validateLinks	= require('./lib/index.js')
 
 mdlinks(args[0], args[1])
   .then(obj => console.log(obj))
