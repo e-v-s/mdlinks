dcf9a913d27edefe26ebbd6578b1b02104c43c93
evs <evoraleite@gmail.com>
2019-12-03 14:50:18 -0300

    tentando fazer o fetch

--- cli.js
+++ cli.js
@@ -3,5 +3,14 @@ const args = process.argv.slice(2)
 const mdlinks = require('./lib/index.js')
 
 mdlinks(args[0], args[1])
-  .then(obj => console.log(obj))
+  .then((obj) => {
+  	if (args[1]) {
+  		if (args[1] === '--validate') {
+  			let option = { validate: true }
+  		}
+  		console.log(obj)
+  	} else {
+  		console.log(obj)
+  	}
+  })
   .catch(err => console.log(err))
