3aaba0e8d72c14c0980a0a6a41b9b3a365b624e9
evs <evoraleite@gmail.com>
2019-12-03 15:20:48 -0300

    OK

--- cli.js
+++ cli.js
@@ -2,6 +2,6 @@
 const args = process.argv.slice(2)
 const mdlinks = require('./lib/index.js')
 
-mdlinks(args[0], args[1])
+mdlinks(args[0])
   .then(obj => console.log(obj))
   .catch(err => console.log(err))
