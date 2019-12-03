08d15e5561dd0a5b0e7d4e26522b88bc934c6914
evs <evoraleite@gmail.com>
2019-12-03 16:40:43 -0300

    hacker-edition: done

--- cli.js
+++ cli.js
@@ -4,13 +4,11 @@ const mdlinks = require('./lib/index.js')
 
 mdlinks(args[0], args[1])
   .then((obj) => {
-  	if (args[1]) {
-  		if (args[1] === '--validate') {
-  			let option = { validate: true }
-  		}
-  		console.log(obj)
-  	} else {
-  		console.log(obj)
-  	}
+    if (args[1] === '--validate') {
+      args[1] = { validate: true }
+      console.log(obj)
+    } else {
+      console.log(obj)
+    }
   })
   .catch(err => console.log(err))
