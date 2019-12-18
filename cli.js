0fc3713781966afe839902cb18886d7e248959f6
evs <evoraleite@gmail.com>
2019-12-17 23:01:28 -0300

    refatorado

--- cli.js
+++ cli.js
@@ -3,12 +3,18 @@ const args = process.argv.slice(2)
 const mdlinks = require('./lib/index.js')
 
 mdlinks(args[0], args[1])
-  .then((obj) => {
-    if (args[1] === '--validate') {
-      args[1] = { validate: true }
-      console.log(obj)
-    } else {
-      console.log(obj)
-    }
-  })
-  .catch(err => console.log(err))
+ .then((obj) => {
+   if (args[1] === '--validate') {
+   	obj.forEach(i => {
+   		console.log('text:', i.text, 'href:', i.href, 'status:', (i.status).toString())
+   	})
+   } else if (args[1]) {
+   	console.log("Command not found. Did you mean '--validate'?")
+   } else {
+   	obj.forEach(i => {
+   		console.log('text:', i.text, 'href:', i.href)
+   	})
+   }
+ })
+ .catch(err => console.log(err))
+
